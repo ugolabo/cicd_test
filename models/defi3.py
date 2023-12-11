6d2abc879b5431a51b79a065e540d5c7361bad59
hugues <hsanterre1@gmail.com>
2023-12-11 11:26:40 -0500

    Modifier requirements.txt avec des commentaires; modifier defi3.py pour le rendre conforme à flake8

--- models/defi3.py
+++ models/defi3.py
@@ -1,4 +1,6 @@
-# COMPTEUR DE SCRABBLE
+"""
+COMPTEUR DE SCRABBLE
+"""
 
 # Voici la clé de correspondance (un dictionnaire)
 # lettre:valeur pour calculer le score d'un mot
@@ -7,6 +9,7 @@ score = {"a": 1, "b": 3, "c": 3, "d": 2, "e": 1, "f": 4,
          "m": 3, "n": 1, "o": 1, "p": 3, "q": 10, "r": 1,
          "s": 1, "t": 1, "u": 1, "v": 4, "w": 4, "x": 8,
          "y": 4, "z": 10}
+
 
 # Créer une fonction pour
 # calculer le score du mot
@@ -23,24 +26,25 @@ def calculer_score(mot):
     points = 0
     # Pour chaque lettre l dans
     # la chaine de caractères mot2
-    for l in mot2:
-        # Si la lettre l égale 
+    for lettre in mot2:
+        # Si la lettre l égale
         # la lettre l convertie en chaine d'un caractère avec
         # str() pour string
-		# Ce contrôle de qualité
+        # Ce contrôle de qualité
         # élimine les chiffres, car ils sont
         # convertir en texte (1 devient "1" ou '1')
-        if l == str(l):
-            # Extraire la valeur de la lettre l 
+        if lettre == str(lettre):
+            # Extraire la valeur de la lettre
             # du dictionnaire score
             # Ajouter cette valeur à la variable points
             # Chaque itération ajoute une nouvelle valeur
             # À la fin de la boucle, l'objet points
             # a accumulé toutes les valeurs de chaque
             # lettre du mot évalué
-            points += score[l]
+            points += score[lettre]
     # Retourner le total calculé
     return points
+
 
 # Imprimer un espace et un diviseur
 print("")
